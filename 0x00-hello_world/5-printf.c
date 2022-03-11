65ecf26ffa05b76523f977c0903618289bfa298a
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-03-11 02:24:25 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
-/** 
- *main - program startup
+/**
+ * main - program startup
  (*
  *Description: prints the exact phrase followed by a new line)?
  *Return: return (0) is the required function signature
  */
 
-
-
 #include <stdio.h>
-int main(void)
+int main (void)
 {
-	  puts("\"Programming is like building a multilingual puzzle");
-	     return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
