0ffa7d0058d574d84b1ab8e664767da66e1edbc4
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-03-12 03:30:21 -0800

    dfds

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 	int m;
 
-	srand(time(0);
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
 
@@ -22,5 +22,5 @@ int main(void)
 		printf("Last digit of %i is %i and is 0\n", n, m);
 	else
 		printf("last digi of %i is %i and is less than 6 and not 0\n", n, m);
-	return (0);
+	return (0);;
 }
