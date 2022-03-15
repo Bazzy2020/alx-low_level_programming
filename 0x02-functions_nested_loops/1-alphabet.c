03d38c0c7d07046819dffc98bf74a3dbff9dc1c6
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-03-15 12:19:35 -0700

    sfds

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  *Return: Always 0.
  */
 
-int print_alphabet(void)
+int Print_alphabet(void)
 {
 	char Alphas;
 
 	for (Alphas = 'a'; Alphas <= 'z'; Alphas++)
 	{
-		_putchar(Alphas);
+		putchar(Alphas);
 	}
 	_putchar('\n');
 	return (0);
