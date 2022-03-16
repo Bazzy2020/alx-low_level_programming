b7960a3a93bd469fd68b326292e1772b1ddbcfd9
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-03-16 09:14:24 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs - short description, single line
+ *@n: contains value
+ *Return: returns absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	return (n);
+}
