e62364ab201994371de6eb2f28be99760ed09091
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-03-16 07:02:03 -0700

    sdfsf

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,13 @@
  * Description: uses for function
  */
 
-void print_alphabet(void)
+int print_alphabet(void)
 {
-	char ch;
-	void _putchar(int);
-	void print_alphabet();
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+return (0);
 }
