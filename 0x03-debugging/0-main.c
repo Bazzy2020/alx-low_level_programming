9ba73d93e8eee4ac6f041cc24bf945916056a364
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-03-17 13:51:30 -0700

    asdfs

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,14 @@
 
 /**
  *main - tests function that prints if integer is positive or negative
- *Return: Always 0 (success)
+ *Return: Always 0
  */
 
 int main(void)
 {
-	int i;
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+int i;
+
+i = 0;
+positive_or_negative(i);
+return (0);
 }
