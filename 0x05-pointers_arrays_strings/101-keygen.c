778fdbe0850957cce695822b2719bf0669cc746d
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-03-22 13:24:28 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-	int sum;
-	char c;
+int sum;
+char c;
 
-	srand(time(NULL));
+srand(time(NULL));
 
-	while (sum <= 2645)
-	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
-	return (0);
+while (sum <= 2645)
+{
+c = rand() % 128;
+sum += c;
+putchar(c);
+}
+putchar(2772 - sum);
+return (0);
 }
