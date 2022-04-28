8fa36991a32409240e54d2fa93c5562fba314895
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-04-28 06:47:08 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,10 +4,10 @@
  *free_listint - The function that free a memory location
  *@head: the head of the linked list
  */
-
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
+
 	if (head == NULL)
 		return;
 	while (head)
